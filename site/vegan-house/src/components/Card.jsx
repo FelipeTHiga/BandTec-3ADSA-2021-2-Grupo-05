290bfc7ec97f70cabc6db7e03b74783183192eb8
rafael-rochaalmeida <rafael.almeida@bandtec.com.br>
2021-10-13 22:10:38 -0300

    VH-00 add logica cards da home

--- site/vegan-house/src/components/Card.jsx
+++ site/vegan-house/src/components/Card.jsx
@@ -1,27 +1,38 @@
 import shoe from '../assets/images/shoe.png'
 import star from '../assets/images/star.png'
 import '../styles/card.scss'
-export function Card() {
+
+import { newsProducts } from '../scripts/vetor.js';
+
+const creatCard = (card, index) => {
     return (
-        <div className="card-product line-up">
-            <img src={shoe} />
-            <div className="container-evaluation-card line-up">
-                <div className="container-stars line-up">
-                    <img src={star} />
-                    <img src={star} />
-                    <img src={star} />
-                    <img src={star} />
-                    <img src={star} />
-                    <div className="container-note line-up">
-                        <p>5</p>
-                    </div>
+        <div className="card-product line-up" key={index}>
+        <img src={shoe} />
+        <div className="container-evaluation-card line-up">
+            <div className="container-stars line-up">
+                <img src={star} />
+                <img src={star} />
+                <img src={star} />
+                <img src={star} />
+                <img src={star} />
+                <div>{card.note}</div>
+                <div className="container-note line-up">
+                    <p>5</p>
                 </div>
-           </div>
-           <div className="container-description-product">
-                <p className="description">Descrição de alguma coisa</p>
-                <p className="price">R$50,00</p>
-                <button><i className="fa fa-shopping-cart"></i>Comprar</button>
-           </div>
-        </div>
+            </div>
+       </div>
+       <div className="container-description-product">
+            <p className="description">{card.description}</p>
+            <p className="price">R${card.price}</p>
+            <button><i className="fa fa-shopping-cart"></i>Comprar</button>
+       </div>
+    </div>
+    )
+}
+
+
+ export function Card() {
+    return (
+        newsProducts.map(creatCard)
     )
 }
