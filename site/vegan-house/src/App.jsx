a5a1a17ee23805835745ccac71f916423609d1ee
vitoriadesouzasantos <vitoria.santos@bandtec.com.br>
2021-10-20 11:40:38 -0300

    Início tela de catalogo

--- site/vegan-house/src/App.jsx
+++ site/vegan-house/src/App.jsx
@@ -5,8 +5,9 @@ import { MyProducts } from "./pages/MyProducts";
 import { ProductPage } from "./pages/ProductPage";
 import { Register } from "./pages/Register";
 import { RegisterSeller } from "./pages/RegisterSeller";
-import { MyProducts } from "./pages/MyProducts";
+// import { MyProducts } from "./pages/MyProducts";
 import { ShoppingCart } from "./pages/ShoppingCart";
+import { Catalog } from "./pages/Catalog";
 
 
 // import { NewRoom } from "./pages/NewRoom";
@@ -21,8 +22,9 @@ function App() {
         <Route path="/perfil/cadastro-seller" component={RegisterSeller} />
         <Route path="/login" component={Login} />
         {/* /todos os resultados/{props.categoria}/{props.subcategoria}/{props.nomeDoProduto} */}
-        <Route path="/perfil/meus-produtos" component={MyProducts} />
+        {/* <Route path="/perfil/meus-produtos" component={MyProducts} /> */}
         <Route path="/carrinho" component={ShoppingCart} />
+        <Route path="/catalogo" component={Catalog} />
       </Switch>
     </BrowserRouter>
   );
---
+++ site/vegan-house/src/pages/Catalog.jsx
@@ -0,0 +1,133 @@
+import { Navbar } from '../components/Navbar';
+import { Footer } from '../components/Footer';
+import { Submenu } from '../components/Submenu';
+import '../styles/catalog.scss';
+import '../styles/global.scss';
+import { getUser } from '../scripts/crud-user';
+
+export function Catalog() {
+    return (
+        <>
+            <Navbar isLogged={getUser} />
+            <Submenu />
+            <section class="container-search-result">
+
+                <div className="title-catalog">
+                    <h1>Todos os resultados/</h1>
+                </div>
+
+                <div className="catalog-content">
+
+                    <div className="container-categories">
+                        <div className="results-title">
+                            <h3>resultado(s)</h3>
+                        </div>
+                        <div class="categories">
+                            <h3 class="category-title">Categorias</h3>
+                            <ul class="category-list">
+                                <li id="item-0" class="category-itens" onclick="toggleClass('item-0', 'iten-active')">Todos
+                                    (<div class="result-number-2"></div>)</li>
+                                <li id="item-1" class="category-itens" onclick="toggleClass('item-1', 'iten-active')">Acessórios
+                                    (<div class="result-acessories"></div>)</li>
+                                <li id="item-2" class="category-itens" onclick="toggleClass('item-2', 'iten-active')">Alimentos
+                                    (<div class="result-food"></div>)</li>
+                                <li id="item-3" class="category-itens" onclick="toggleClass('item-3', 'iten-active')">Cosméticos
+                                    (<div class="result-cosmetics"></div>)</li>
+                                <li id="item-4" class="category-itens" onclick="toggleClass('item-4', 'iten-active')">Saúde
+                                    (<div class="result-health"></div>)</li>
+                                <li id="item-5" class="category-itens" onclick="toggleClass('item-5', 'iten-active')">Vestimenta
+                                    (<div class="result-clothing"></div>)</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="container-filters">
+                            <div class="order">
+                                <h3>Ordenar por:</h3>
+                            </div>
+                            <div class="filters">
+                                <button id="btn-filter-1" class="btn-filter"
+                                    onclick="toggleClass('btn-filter-1', 'active')">Menor preço</button>
+                                <button id="btn-filter-2" class="btn-filter"
+                                    onclick="toggleClass('btn-filter-2', 'active')">Maior preço</button>
+                                <button id="btn-filter-3" class="btn-filter"
+                                    onclick="toggleClass('btn-filter-3', 'active')">Nome</button>
+                            </div>
+
+                        </div>
+                </div>
+
+                {/* <div class="second-section">
+                    <div class="left-column">
+                        <div class="total-results">
+                            <h3 class="result-title">
+                                <div class="result-number"></div>resultado(s)
+                            </h3>
+                        </div>
+                    </div>
+
+                    <div class="right-column">
+                        <div class="container-filters">
+                            <div class="order">
+                                <h3>Ordenar por:</h3>
+                            </div>
+
+                            <div class="filters">
+                                <button id="btn-filter-1" class="btn-filter"
+                                    onclick="toggleClass('btn-filter-1', 'active')">Menor preço</button>
+                                <button id="btn-filter-2" class="btn-filter"
+                                    onclick="toggleClass('btn-filter-2', 'active')">Maior preço</button>
+                                <button id="btn-filter-3" class="btn-filter"
+                                    onclick="toggleClass('btn-filter-3', 'active')">Nome</button>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="third-section">
+
+                    <div class="left-column">
+                        <div class="container-categories">
+                            <h3 class="category-title">Categorias</h3>
+                            <ul class="category-list">
+                                <li id="item-0" class="category-itens" onclick="toggleClass('item-0', 'iten-active')">Todos
+                                    (<div class="result-number-2"></div>)</li>
+                                <li id="item-1" class="category-itens" onclick="toggleClass('item-1', 'iten-active')">Acessórios
+                                    (<div class="result-acessories"></div>)</li>
+                                <li id="item-2" class="category-itens" onclick="toggleClass('item-2', 'iten-active')">Alimentos
+                                    (<div class="result-food"></div>)</li>
+                                <li id="item-3" class="category-itens" onclick="toggleClass('item-3', 'iten-active')">Cosméticos
+                                    (<div class="result-cosmetics"></div>)</li>
+                                <li id="item-4" class="category-itens" onclick="toggleClass('item-4', 'iten-active')">Saúde
+                                    (<div class="result-health"></div>)</li>
+                                <li id="item-5" class="category-itens" onclick="toggleClass('item-5', 'iten-active')">Vestimenta
+                                    (<div class="result-clothing"></div>)</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="container-products">
+                        <div class="container-products-and-arrow line-up new-style">
+
+                            <div class="container-cards-products line-up teste">
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="fourth-section right-column">
+                    <div class="container-pages">
+                        <h3 class="page-number">${ }</h3>
+                        <i class="fas fa-caret-right arrow"></i>
+                    </div>
+                </div> */}
+
+
+            </section>
+            <Footer />
+        </>
+    )
+}
