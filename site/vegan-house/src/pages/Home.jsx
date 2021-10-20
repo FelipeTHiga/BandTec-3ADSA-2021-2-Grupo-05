ede7edfae7aab81ca13de10ec68b83395f795507
rafael-rochaalmeida <rafael.almeida@bandtec.com.br>
2021-10-20 18:40:24 -0300

    VH-00 Atualizacao pag home

--- site/vegan-house/src/pages/Home.jsx
+++ site/vegan-house/src/pages/Home.jsx
@@ -3,6 +3,9 @@ import { Submenu } from "../components/Submenu";
 import { Footer } from "../components/Footer";
 import { Button } from '../components/Button';
 import { Card } from "../components/Card";
+import { CardSeller } from "../components/CardSellers";
+import '../styles/carousel.scss'
+
 import '../styles/home.scss';
 
 export function Home() {
@@ -23,6 +26,9 @@ export function Home() {
                    <div className="container-img-banner"></div>
                    
        </section>
+
+
+
        <section className="container-pattern container-products">
             <div className="container-title-pattern line-up">
                 <div className="line-average"></div>
@@ -32,16 +38,60 @@ export function Home() {
 
             
             <div className="container-products-and-arrow line-up">
-                <i className="fas fa-caret-left arrow"></i>
-                    <div className="container-cards-products line-up">
+                <i  className="fas fa-caret-left arrow"></i>
+                    <div  className="container-cards-products line-up">
                         <Card />
-                    
+                        
                     </div>
                 <i className="fas fa-caret-right arrow"></i>
             </div>
 
               <div className="line-great"></div>
     </section>
+
+
+
+    {/* <!-- incio container de vendedores --> */}
+    <section class="container-pattern container-sellers-pop line-up">
+        <div class="container-title-pattern line-up">
+            <div class="line-average"></div>
+                <div class="container-title-sellers-pop">
+                    <p>Vendedores</p> <p> Populares</p>
+                </div>
+            
+            <div class="line-average"></div>
+        </div>
+
+        <div class="container-cards-sellers line-up">
+            <CardSeller />
+        </div>
+
+        <div class="line-great"></div>
+    </section>
+    {/* <!-- fim container de vendedores --> */}
+
+
+
+
+    {/* <!-- inicio container de produtos 2--> */}
+    <section class="container-pattern container-products line-up">
+        <div class="container-title-pattern line-up">
+            <div class="line-average"></div>
+                <p>Destaques</p>
+            <div class="line-average"></div>
+        </div>
+
+        <div class="container-products-and-arrow line-up">
+            <i class="fas fa-caret-left arrow"></i>
+                <div class="container-cards-products products-highlights line-up">
+                <Card />
+                </div>
+            <i class="fas fa-caret-right arrow"><a href="./"></a></i>
+        </div>
+
+              <div class="line-great"></div>
+    </section>
+    {/* <!-- fim container de produtos 2--> */}
 
         <Footer/>
         </>
