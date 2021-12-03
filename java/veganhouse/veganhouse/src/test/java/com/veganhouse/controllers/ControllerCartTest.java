e5f19a1ae40a54a2b46b7427f3960c61c5ecc341
larissaElMajdobLima <larissa.lima@bandtec.com.br>
2021-12-03 13:43:29 -0300

    VH-00 - Ajustes do Back-end

---
+++ java/veganhouse/veganhouse/src/test/java/com/veganhouse/controllers/ControllerCartTest.java
@@ -0,0 +1,139 @@
+package com.veganhouse.controllers;
+
+
+import com.veganhouse.domain.Cart;
+import com.veganhouse.domain.Product;
+import com.veganhouse.repository.IProductRepository;
+import com.veganhouse.repository.IUserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest(classes = {ControllerCart.class, IProductRepository.class})
+public class ControllerCartTest {
+
+    @Autowired
+    ControllerCart controller;
+
+
+    @MockBean
+    private IProductRepository productRepository;
+
+    @MockBean
+    private IUserRepository userRepository;
+
+
+
+
+    private Cart cart = new Cart();
+
+
+    @Test
+    @DisplayName("postProduct() deve retornar 'status 404'")
+    void addProductCartDeveRetornarStatus404SemCorpo(){
+
+        Integer idTeste = 10;
+
+        Product product = new Product(20,
+                "Camisa de Algodão",
+                20.2,
+                "Vestimenta",
+                "Vestimenta",
+                "Camisa confortável ideal para dias quentes",
+                20,
+                1);
+
+        when(productRepository.existsById(idTeste)).thenReturn(false);
+
+        ResponseEntity  resposta = controller.addProductCart(mock(Product.class));
+
+        assertEquals(404, resposta.getStatusCodeValue());
+
+    }
+
+    @Test
+    @DisplayName("removeProductCart() deve retornar sem corpo 'status 404'")
+    void removeProductCartDeveRetornarStatus200SemCorpo(){
+        Integer idTeste = 10;
+
+        Product product = new Product(20,
+                "Camisa de Algodão",
+                20.2,
+                "Vestimenta",
+                "Vestimenta",
+                "Camisa confortável ideal para dias quentes",
+                20,
+                1);
+
+       when(productRepository.existsById(idTeste)).thenReturn(true);
+
+       when(productRepository.getById(idTeste)).thenReturn(product);
+
+        ResponseEntity responsta = controller.removeProductCart(product);
+
+        assertEquals(404, responsta.getStatusCodeValue());
+
+    }
+
+
+    @Test
+    @DisplayName("addMultiProductCart() deve retornar status 404 sem corpo")
+    void addMultiProductCartDeveRetornarStatus404SemCorpo(){
+        Integer idTeste = 10;
+
+        Product product = new Product(20,
+                "Camisa de Algodão",
+                20.2,
+                "Vestimenta",
+                "Vestimenta",
+                "Camisa confortável ideal para dias quentes",
+                20,
+                1);
+
+        when(productRepository.existsById(idTeste)).thenReturn(true);
+
+        ResponseEntity  resposta = controller.addMultiProductCart(product, 20);
+
+        assertEquals(404, resposta.getStatusCodeValue());
+
+    }
+
+
+    @Test
+    void removeMultiProductCartDeveRetornarStatus200SemCorpo(){
+
+        Integer idTeste = 10;
+
+        Product product = new Product(20,
+                "Camisa de Algodão",
+                20.2,
+                "Vestimenta",
+                "Vestimenta",
+                "Camisa confortável ideal para dias quentes",
+                20,
+                1);
+
+        when(productRepository.existsById(idTeste)).thenReturn(true);
+
+        cart.removeProduct(productRepository.getById(idTeste));
+
+        ResponseEntity resposta = controller.removeMultiProductCart(product, 20);
+
+        assertEquals(404, resposta.getStatusCodeValue());
+    }
+
+
+
+
+
+
+}
---
+++ java/veganhouse/veganhouse/src/test/java/com/veganhouse/controllers/ControllerSellerTest.java
@@ -0,0 +1,83 @@
+package com.veganhouse.controllers;
+
+
+import com.veganhouse.domain.Seller;
+import com.veganhouse.domain.User;
+import com.veganhouse.repository.ISellerRepository;
+import com.veganhouse.repository.IUserRepository;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.veganhouse.repository.ISellerCertified;
+
+
+
+import com.veganhouse.repository.IAdressRepository;
+import com.veganhouse.repository.ISellerRepository;
+import com.veganhouse.repository.IUserRepository;
+import org.springframework.boot.test.context.SpringBootTest;
+//port org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest(classes = {ControllerSeller.class, ISellerRepository.class, IUserRepository.class})
+public class ControllerSellerTest {
+
+    @org.springframework.beans.factory.annotation.Autowired(required=true)
+    private ISellerRepository sellerRepository;
+
+    @MockBean
+    private IUserRepository userRepository;
+
+    @Autowired
+    private ControllerSeller controller;
+
+    @Test
+    void getSellerDeveRetornarStatus200SemCorpo(){
+        Integer idTeste = 10;
+        Integer idFkTeste = 20;
+
+
+        User user = mock(User.class);
+
+        when(sellerRepository.findByFkUser(idFkTeste)).thenReturn(mock(Seller.class));
+
+
+        ResponseEntity resposta = controller.getSeller(idTeste);
+
+
+        assertEquals(200, resposta.getStatusCodeValue());
+    }
+
+
+    @Test
+    void updateSellerDeveRetornarStatus200SemCorpo(){
+
+        Integer idTeste = 10;
+        when(sellerRepository.existsById(idTeste)).thenReturn(true);
+
+        Seller seller = new Seller();
+        seller.setIdSeller(idTeste);
+        sellerRepository.save(seller);
+
+       ResponseEntity resposta = controller.updateSeller(idTeste, seller);
+
+       assertEquals(200, resposta.getStatusCodeValue());
+
+    }
+
+
+}
