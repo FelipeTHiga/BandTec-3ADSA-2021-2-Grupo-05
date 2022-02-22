85f14c1f46b920c5b978b447cdde22404f879317
vitoriadesouzasantos <vitoria.santos@bandtec.com.br>
2022-02-21 22:03:04 -0300

    VH-198

--- site/vegan-house/src/components/OrderBox.jsx
+++ site/vegan-house/src/components/OrderBox.jsx
@@ -1,6 +1,15 @@
 import "../styles/orderBox.scss"
 import { ProductOrder } from "./ProductOrder"
 export function OrderBox(props) {
+    function color() {
+        if(props.status === "Pagamento aprovado") {
+            return "p-status-green"
+        } else if(props.status === "Pagamento rejeitado") {
+            return "p-status-red"
+        } else {
+            return "p-status-blue"
+        }
+    }
     return (
         <>
             <div className="order-box">
@@ -19,7 +28,7 @@ export function OrderBox(props) {
                     </div>
                     <div className="status-order">
                         <h2>Status</h2>
-                        <p className="p-status">{props.status}</p>
+                        <p className={`p-status ${color()}`}>{props.status}</p>
                     </div>
                 </div>
             </div>
