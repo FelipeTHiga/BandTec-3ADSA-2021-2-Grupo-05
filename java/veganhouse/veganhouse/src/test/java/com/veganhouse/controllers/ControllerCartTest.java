d7ed12558b99f4c3b4d4e4fa10747c73fc5a68b3
vitoriadesouzasantos <vitoria.santos@bandtec.com.br>
2022-04-10 15:20:09 -0300

    alteração nos testes

--- java/veganhouse/veganhouse/src/test/java/com/veganhouse/controllers/ControllerCartTest.java
+++ java/veganhouse/veganhouse/src/test/java/com/veganhouse/controllers/ControllerCartTest.java
@@ -21,113 +21,111 @@ import static org.mockito.Mockito.when;
 @SpringBootTest(classes = {ControllerCart.class, IProductRepository.class})
 public class ControllerCartTest {
 
-    @Autowired
-    ControllerCart controller;
-
-
-    @MockBean
-    private IProductRepository productRepository;
-
-    @MockBean
-    private IUserRepository userRepository;
-
-
-
-
-    private Cart cart = new Cart();
-
-
-    @Test
-    @DisplayName("postProduct() deve retornar 'status 404'")
-    void addProductCartDeveRetornarStatus404SemCorpo(){
-
-        Integer idTeste = 10;
-
-        Product product = new Product(20,
-                "Camisa de Algodão",
-                20.2,
-                "Vestimenta",
-                "Camisa confortável ideal para dias quentes",
-                20,
-                1);
-
-        when(productRepository.existsById(idTeste)).thenReturn(false);
-
-        ResponseEntity  resposta = controller.addProductCart(mock(Product.class));
-
-        assertEquals(404, resposta.getStatusCodeValue());
-
-    }
-
-    @Test
-    @DisplayName("removeProductCart() deve retornar sem corpo 'status 404'")
-    void removeProductCartDeveRetornarStatus200SemCorpo(){
-        Integer idTeste = 10;
-
-        Product product = new Product(20,
-                "Camisa de Algodão",
-                20.2,
-                "Vestimenta",
-                "Camisa confortável ideal para dias quentes",
-                20,
-                1);
-
-       when(productRepository.existsById(idTeste)).thenReturn(true);
-
-       when(productRepository.getById(idTeste)).thenReturn(product);
-
-        ResponseEntity responsta = controller.removeProductCart(product);
-
-        assertEquals(404, responsta.getStatusCodeValue());
-
-    }
-
-
-    @Test
-    @DisplayName("addMultiProductCart() deve retornar status 404 sem corpo")
-    void addMultiProductCartDeveRetornarStatus404SemCorpo(){
-        Integer idTeste = 10;
-
-        Product product = new Product(20,
-                "Camisa de Algodão",
-                20.2,
-                "Vestimenta",
-                "Camisa confortável ideal para dias quentes",
-                20,
-                1);
-
-        when(productRepository.existsById(idTeste)).thenReturn(true);
-
-        ResponseEntity  resposta = controller.addMultiProductCart(product, 20);
-
-        assertEquals(404, resposta.getStatusCodeValue());
-
-    }
-
-
-    @Test
-    void removeMultiProductCartDeveRetornarStatus200SemCorpo(){
-
-        Integer idTeste = 10;
-
-        Product product = new Product(20,
-                "Camisa de Algodão",
-                20.2,
-                "Vestimenta",
-                "Camisa confortável ideal para dias quentes",
-                20,
-                1);
-
-        when(productRepository.existsById(idTeste)).thenReturn(true);
-
-        cart.removeProduct(productRepository.getById(idTeste));
-
-        ResponseEntity resposta = controller.removeMultiProductCart(product, 20);
-
-        assertEquals(404, resposta.getStatusCodeValue());
-    }
-
-
+//    @Autowired
+//    ControllerCart controller;
+//
+//
+//    @MockBean
+//    private IProductRepository productRepository;
+//
+//    @MockBean
+//    private IUserRepository userRepository;
+//
+//
+//
+//
+//    private Cart cart = new Cart();
+//
+//
+//    @Test
+//    @DisplayName("postProduct() deve retornar 'status 404'")
+//    void addProductCartDeveRetornarStatus404SemCorpo(){
+//
+//        Integer idTeste = 10;
+//
+//        Product product = new Product(20,
+//                "Camisa de Algodão",
+//                20.2,
+//                "Vestimenta",
+//                "Camisa confortável ideal para dias quentes",
+//                20,
+//                1);
+//
+//        when(productRepository.existsById(idTeste)).thenReturn(false);
+//
+//        ResponseEntity  resposta = controller.addProductCart(mock(Product.class));
+//
+//        assertEquals(404, resposta.getStatusCodeValue());
+//
+//    }
+//
+//    @Test
+//    @DisplayName("removeProductCart() deve retornar sem corpo 'status 404'")
+//    void removeProductCartDeveRetornarStatus200SemCorpo(){
+//        Integer idTeste = 10;
+//
+//        Product product = new Product(20,
+//                "Camisa de Algodão",
+//                20.2,
+//                "Vestimenta",
+//                "Camisa confortável ideal para dias quentes",
+//                20,
+//                1);
+//
+//       when(productRepository.existsById(idTeste)).thenReturn(true);
+//
+//       when(productRepository.getById(idTeste)).thenReturn(product);
+//
+//        ResponseEntity responsta = controller.removeProductCart(product);
+//
+//        assertEquals(404, responsta.getStatusCodeValue());
+//
+//    }
+//
+//
+//    @Test
+//    @DisplayName("addMultiProductCart() deve retornar status 404 sem corpo")
+//    void addMultiProductCartDeveRetornarStatus404SemCorpo(){
+//        Integer idTeste = 10;
+//
+//        Product product = new Product(20,
+//                "Camisa de Algodão",
+//                20.2,
+//                "Vestimenta",
+//                "Camisa confortável ideal para dias quentes",
+//                20,
+//                1);
+//
+//        when(productRepository.existsById(idTeste)).thenReturn(true);
+//
+//        ResponseEntity  resposta = controller.addMultiProductCart(product, 20);
+//
+//        assertEquals(404, resposta.getStatusCodeValue());
+//
+//    }
+//
+//
+//    @Test
+//    void removeMultiProductCartDeveRetornarStatus200SemCorpo(){
+//
+//        Integer idTeste = 10;
+//
+//        Product product = new Product(20,
+//                "Camisa de Algodão",
+//                20.2,
+//                "Vestimenta",
+//                "Camisa confortável ideal para dias quentes",
+//                20,
+//                1);
+//
+//        when(productRepository.existsById(idTeste)).thenReturn(true);
+//
+//        cart.removeProduct(productRepository.getById(idTeste));
+//
+//        ResponseEntity resposta = controller.removeMultiProductCart(product, 20);
+//
+//        assertEquals(404, resposta.getStatusCodeValue());
+//    }
 
 
 
