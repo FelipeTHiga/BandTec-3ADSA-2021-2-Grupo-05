00a8bd4722d8ca5f6e143c065f8f5154abec9375
rafael <rafael.almeida@bandtec.com.br>
2022-05-21 23:07:03 -0300

    atualizacao base URL

--- site/vegan-house/src/services/api.js
+++ site/vegan-house/src/services/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
     //baseURL: 'http://localhost:8080/'
-    baseURL: 'http://174.129.13.249:8080/'
+    // baseURL: 'http://174.129.13.249:8080/'
+    baseURL: 'http://34.205.210.155:8080/'
 });
 
 export default api;
