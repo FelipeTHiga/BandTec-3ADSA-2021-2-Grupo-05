c157cb6c973da6f88b6516bded2496f2b94a1c27
root <root@web1.ddns.net>
2022-05-31 05:23:12 +0000

    base url api.js

--- site/vegan-house/src/services/api.js
+++ site/vegan-house/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const api = axios.create({
-<<<<<<< HEAD
     //baseURL: 'http://localhost:8080/'
     // baseURL: 'http://174.129.13.249:8080/'
     baseURL: 'https://veganhouseback.ddns.net'
