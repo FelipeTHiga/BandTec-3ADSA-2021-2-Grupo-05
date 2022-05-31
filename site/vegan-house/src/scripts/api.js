bdac6214b0e2e6e4c72edd53e1ae28b4f43e864f
Miguel Nunes <miguel.assuncao@linx3.com>
2022-05-31 00:16:07 -0300

    vh-00 ajustes image

--- site/vegan-house/src/scripts/api.js
+++ site/vegan-house/src/scripts/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://174.129.13.249:8080/'
+    baseURL: 'https://veganhouseback.ddns.net/'
 });
 
 export default api;
--- site/vegan-house/src/services/api.js
+++ site/vegan-house/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    //baseURL: 'http://localhost:8080/'
-    baseURL: 'http://174.129.13.249:8080/'
+    baseURL: 'https://veganhouseback.ddns.net/'
+    //baseURL: 'http://174.129.13.249:8080/'
 });
 
 export default api;
